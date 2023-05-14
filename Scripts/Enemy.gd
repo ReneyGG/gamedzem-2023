8713f75d019dcbe1e99b492dc2b0fad9f9b6a431
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-14 19:55:53 +0200

    Enemy V2

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,18 +1,34 @@
 extends KinematicBody2D
 
-
-var gravity = 35
-var acceleration = 2000
-var deacceleration = 2000
-var max_speed = 50
-var friction = 2000
-
-onready var sprite = get_node("Sprite")
-onready var timer = get_node("Timer")
+var gravity = 200
+var speed = 40
+var max_speed = 100
+var motion = Vector2()
+var player = null
+var direction = -1
 
 
-func _process(delta):
-	position += Vector2.LEFT * max_speed * delta
-
+func _physics_process(delta):
+	motion.y += gravity * delta
+#	if player:
+#		motion = position.direction_to((player.position * speed * delta))
+	if motion.x >= (max_speed * -1) && direction == -1:
+		motion.x += speed * direction * delta
+	elif motion.x <= max_speed && direction == 1:
+		motion.x += speed * direction * delta
+	print(motion.x)
+	motion = move_and_slide(motion)
+	enemy()
 	
 
+func enemy():
+	if $Sprite/RayLeft.is_colliding() && direction == -1:
+		motion.x = 0.0
+		direction = direction * -1
+		get_node("Sprite").set_flip_h(true)
+		print("change_dirrection Left")
+	elif $Sprite/RayRight.is_colliding() && direction == 1:
+		motion.x = 0.0
+		direction = direction * -1
+		get_node("Sprite").set_flip_h(false)
+		print("change_dirrection Right")
