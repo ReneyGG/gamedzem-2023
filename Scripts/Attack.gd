c2890a16d7fd2c4aacdc5b304cfe7c840f047f23
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-15 13:39:17 +0200

    Gotowy przeciwnik

--- Scripts/Attack.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-const FIRE_DAMAGE = 10
-
-func _physics_process(delta):
-	for body in get_overlapping_bodies():
-		if body.has_method("take_damage"):
-			body.take_damage(FIRE_DAMAGE * delta)
