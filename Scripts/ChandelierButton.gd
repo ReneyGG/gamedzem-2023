a3ce8db757a070559111480179cfa62b6dc7877e
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-20 13:03:48 +0200

    BOSS

--- Scripts/ChandelierButton.gd
+++ Scripts/ChandelierButton.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+
+export(NodePath) var leads = "res://Scenes/Main.tscn"
+
 func activate():
-	get_parent().get_node("Chandelier").activate()
+#	get_parent().get_node("Chandelier").activate()
+	get_node(leads).activate()
 	self.monitorable = false
