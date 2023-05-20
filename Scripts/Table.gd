13e46f2ca13e1c9cefe3664068a23bf86db60c92
ReneyGG <jakubczyzycki12@gmail.com>
2023-05-20 11:15:34 +0200

    added highlight and tutotorial

--- Scripts/Table.gd
+++ Scripts/Table.gd
@@ -1,11 +1,21 @@
 class_name Table extends Area2D
 
 var activated
+var hl
 
 func _ready():
 	activated = false
+	hl = false
 	$Crouch1.hide()
 
 func activate():
 	activated = !activated
 	$Crouch1.visible = activated
+
+func highlight():
+	print("hl")
+	hl = !hl
+	if hl:
+		$Sprite.frame = 1
+	else:
+		$Sprite.frame = 0
